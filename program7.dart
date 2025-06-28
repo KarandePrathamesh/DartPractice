2c62a660316a4816d450d4baa2ad5791f4514dbd
Prathamesh Karande <148135010+KarandePrathamesh@users.noreply.github.com>
2025-06-28 14:12:40 +0000

    Future Async Await

---
+++ program7.dart
@@ -0,0 +1,31 @@
+/**
+    Futures, Async, Await
+ */
+ void main() async {
+
+    // Futures --> like promises in javascript 
+    //            can have uncompleted or completed state
+
+    // fetchPost().then((p){
+    //     print(p.title);
+    //     print(p.userId);
+    // });
+
+    final post = await fetchPost();
+    print(post.title);
+    print(post.userId);
+ }
+
+ Future<Post> fetchPost(){
+    const delay = Duration(seconds: 3);
+    return Future.delayed(delay,(){
+        return Post("my post",123);
+    });
+ }
+
+ class Post{
+    String? title;
+    int? userId;
+    Post(this.title, this.userId);
+
+ }
